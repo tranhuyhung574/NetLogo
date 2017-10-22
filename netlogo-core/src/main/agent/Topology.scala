8d21942f03c209a14a16f5fbd5632287069efb01
brotatotes <erichao2018@u.northwestern.edu>
2017-10-21 22:29:14 -0500

    limit getRegion range. tests passing but code needs to be cleaned

--- netlogo-core/src/main/agent/Topology.scala
+++ netlogo-core/src/main/agent/Topology.scala
@@ -3,6 +3,7 @@
 package org.nlogo.agent
 
 import org.nlogo.api, api.AgentException
+import scala.collection.SortedSet
 import java.util.ArrayList
 
 @annotation.strictfp
@@ -77,93 +78,111 @@ extends Neighbors {
   // Box.getPN, the source.pycor gets tested once, and then if Box.getPN calls
   // Topology.getPatchNorth, then source.pycor gets redundantly tested again.
   // - JD, ST 6/3/04
-  
-  def getRegion(X: Double, Y: Double, R: Double): ArrayList[(Int, Int)] = {
-    val w = world.worldWidth
-    val h = world.worldHeight
 
-    val x: Int = X.toInt - world.minPxcor
-    val y: Int = h - 1 - (Y.toInt - world.minPycor)
-    val r: Int = if (X.toInt != X || Y.toInt != Y) R.toInt + 1 else R.toInt
+  def xytoi(x: Int, y: Int): Int = y * world.worldWidth + x
 
-//    var dxmin = 0
-//    var dxmax = 0
-//    var dymin = 0
-//    var dymax = 0
-//
-//    if (world.wrappingAllowedInX) {
-//      val width = w / 2.0
-//      if (r < width) {
-//        dxmax = r
-//        dxmin = -r
-//      }
-//      else {
-//        dxmax = StrictMath.floor(width).toInt
-//        dxmin = -StrictMath.ceil(width - 1).toInt
-//      }
-//    }
-//    else {
-//      val xdiff = world.minPxcor - x
-//      dxmin = if (StrictMath.abs(xdiff) < r) xdiff
-//      else -r
-//      dxmax = StrictMath.min(world.maxPxcor - x, r)
-//    }
-//    if (world.wrappingAllowedInY) {
-//      val height = h / 2.0
-//      if (r < height) {
-//        dymax = r
-//        dymin = -r
-//      }
-//      else {
-//        dymax = StrictMath.floor(height).toInt
-//        dymin = -StrictMath.ceil(height - 1).toInt
-//      }
-//    }
-//    else {
-//      val ydiff = world.minPycor - y
-//      dymin = if (StrictMath.abs(ydiff) < r) ydiff
-//      else -r
-//    }
-//    dymax = StrictMath.min(world.maxPycor - y, r)
+  def xyIterator(x: Int, y: Int, r: Int): Iterator[Int] = {
+    var a: SortedSet[Int] = SortedSet()
+    var xx = -1
+    var yy = -1
 
-
-    val ans: ArrayList[(Int, Int)] = new ArrayList()
-    var (start, end, xx, yy) = (-1, -1, -1, -1)
-
-    val lxx = (x + r) % w
-    val gxx = w - r + x
-    val lyy = (y + r) % h
-    val gyy = h - r + y
-
-    var i = 0
-    val max = h * w
-    while (i < max) {
-      xx = i % w
-      yy = i / w
-
-      if ((Math.abs(x - xx) <= r || (xWraps && ((xx <= lxx) || (xx >= gxx)))) &&
-        (Math.abs(y - yy) <= r || (yWraps && ((yy <= lyy) || (yy >= gyy))))) {
-        if (start == -1) {
-          start = i
-          end = i + 1
-        } else {
-          end += 1
+    for (i <- x - r.min(world.worldWidth-1) to x + r.min(world.worldWidth-1)) {
+      for (j <- y - r.min(world.worldHeight-1) to y + r.min(world.worldHeight-1)) {
+        xx = i
+        yy = j
+        if (!xWraps) {
+          xx = xx.max(0).min(world.worldWidth-1)
+        } else if (xx < 0) {
+          xx = world.worldWidth + xx
+        } else if (xx >= world.worldWidth) {
+          xx %= world.worldWidth
         }
 
-      } else if (start != -1) {
-        ans.add((start, end))
+        if (!yWraps) {
+          yy = yy.max(0).min(world.worldHeight-1)
+        } else if (yy < 0) {
+          yy = world.worldHeight + yy
+        } else if (yy >= world.worldHeight) {
+          yy %= world.worldHeight
+        }
 
-        start = -1
-        end = -1
+//        println((i,j,xx,yy,xytoi(xx,yy)))
+        a += xytoi(xx, yy)
+
+      }
+    }
+
+//    println(a)
+
+    a.iterator
+  }
+  
+  def getRegion(X: Double, Y: Double, R: Double): ArrayList[(Int, Int)] = {
+//    val w = world.worldWidth
+//    val h = world.worldHeight
+
+    val x: Int = X.toInt - world.minPxcor
+    val y: Int = world.worldHeight - 1 - (Y.toInt - world.minPycor)
+    val r: Int = if (X.toInt != X || Y.toInt != Y) R.toInt + 1 else R.toInt
+
+    val ans: ArrayList[(Int, Int)] = new ArrayList()
+//    var (start, end, xx, yy, i) = (-1, -1, -1, -1, 0)
+    var i = 0
+    var start = -1
+    var prev = -1
+
+//    val lxx = (x + r) % w
+//    val gxx = w - r + x
+//    val lyy = (y + r) % h
+//    val gyy = h - r + y
+
+    val it = xyIterator(x, y, r)
+
+//    var i = (y - r).max(0) * w + (x - r).max(0)
+//    val max = (y + r).min(h-1) * w + (x + r).min(w)
+    while (it.hasNext) {
+      i = it.next()
+
+      if (start == -1) {
+        start = i
+      } else if (prev > -1 && i != prev + 1) {
+        ans.add((start, prev + 1))
+        start = i
       }
 
+      prev = i
 
-      i += 1
     }
 
     if (start != -1) {
-      ans.add((start, end))
+      ans.add((start, prev + 1))
     }
+//      xx = i % w
+//      yy = i / w
+
+//      if ((Math.abs(x - xx) <= r || (xWraps && ((xx <= lxx) || (xx >= gxx)))) &&
+//        (Math.abs(y - yy) <= r || (yWraps && ((yy <= lyy) || (yy >= gyy))))) {
+//        if (start == -1) {
+//          start = i
+//          end = i + 1
+//        } else {
+//          end += 1
+//        }
+//
+//      } else if (start != -1) {
+//        ans.add((start, end))
+//
+//        start = -1
+//        end = -1
+//      }
+//
+//
+//      i += 1
+//    }
+
+//    if (start != -1) {
+//      ans.add((start, end))
+//    }
     ans
   }
 
