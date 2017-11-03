40f86f686a6711b1444f2eb36c7216e62456f114
brotatotes <erichao2018@u.northwestern.edu>
2017-11-02 20:01:45 -0500

    merge ranges in getRegionRow

--- netlogo-core/src/main/agent/Topology.scala
+++ netlogo-core/src/main/agent/Topology.scala
@@ -81,29 +81,50 @@ extends Neighbors {
   def xytoi(x: Int, y: Int): Int = y * world.worldWidth + x
 
   def getRegionRow(x: Int, r: Int, offset: Int, arr: ArrayList[(Int, Int)]): Unit = {
-
+    println(arr)
+    println(offset)
 
     if (x-r > 0 && x+r <= world.worldWidth-1) {
-      arr.add((offset + x-r, offset + x+r+1))
+      mergeAdd((offset + x-r, offset + x+r+1), arr)
+//      arr.add((offset + x-r, offset + x+r+1))
 
     } else if (2 * r + 1 >= world.worldWidth) {
       if (xWraps) {
-        arr.add((offset + 0, offset + world.worldWidth))
+        mergeAdd((offset + 0, offset + world.worldWidth), arr)
+//        arr.add((offset + 0, offset + world.worldWidth))
       } else {
-        arr.add((offset + (x - r).max(0), offset + (x + r + 1).min(world.worldWidth)))
+        mergeAdd((offset + (x - r).max(0), offset + (x + r + 1).min(world.worldWidth)), arr)
+//        arr.add((offset + (x - r).max(0), offset + (x + r + 1).min(world.worldWidth)))
       }
 
     } else if (x-r < 0) {
-      arr.add((offset + 0, x + r + 1))
+      mergeAdd((offset + 0, x + r + 1), arr)
+//      arr.add((offset + 0, x + r + 1))
       if (xWraps) {
-        arr.add((offset + world.worldWidth + x - r, offset + world.worldWidth))
+        mergeAdd((offset + world.worldWidth + x - r, offset + world.worldWidth), arr)
+//        arr.add((offset + world.worldWidth + x - r, offset + world.worldWidth))
       }
 
     } else { // x + r >= world.worldWidth
       if (xWraps) {
-        arr.add((offset + 0, offset + x + r - world.worldWidth + 1))
+        mergeAdd((offset + 0, offset + x + r - world.worldWidth + 1), arr)
+//        arr.add((offset + 0, offset + x + r - world.worldWidth + 1))
       }
-      arr.add((offset + x - r, offset + world.worldWidth))
+      mergeAdd((offset + x - r, offset + world.worldWidth), arr)
+//      arr.add((offset + x - r, offset + world.worldWidth))
+    }
+
+    println(arr)
+    println()
+  }
+
+  def mergeAdd(value: (Int, Int), arr: ArrayList[(Int, Int)]): Unit = {
+    val s = arr.size()
+    if (s == 0 || arr.get(s - 1)._2 != value._1) {
+      arr.add(value);
+    } else {
+      val last = arr.get(s - 1)
+      arr.set(s - 1, (last._1, value._2))
     }
   }
 
@@ -120,9 +141,9 @@ extends Neighbors {
     val r: Int = if (X.toInt != X || Y.toInt != Y) R.toInt + 1 else R.toInt
 
     val ans: ArrayList[(Int, Int)] = new ArrayList()
-    val a = Array((-1, -1), (-1, -1))
+    val a = Array((-1, -1), (-1, -1)) // at most 2 possible disjoint y ranges
 
-    if (y-r > 0 && y+r <= world.worldHeight-1) {
+    if (y-r > 0 && y+r <= world.worldHeight-1) { // y range is completely within world
       a(0) = (y-r, y+r+1)
 
     } else if (2*r + 1 >= world.worldHeight) {
@@ -162,8 +183,6 @@ extends Neighbors {
         i += 1
       }
     }
-
-    // foldLeft
 
     ans
   }
