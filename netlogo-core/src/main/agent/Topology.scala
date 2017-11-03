535a4dafa94704afff08f5e74a56162656f6c043
brotatotes <erichao2018@u.northwestern.edu>
2017-11-02 21:03:09 -0500

    all tests passing

--- netlogo-core/src/main/agent/Topology.scala
+++ netlogo-core/src/main/agent/Topology.scala
@@ -78,59 +78,45 @@ extends Neighbors {
   // Topology.getPatchNorth, then source.pycor gets redundantly tested again.
   // - JD, ST 6/3/04
 
-  def xytoi(x: Int, y: Int): Int = y * world.worldWidth + x
-
   def getRegionRow(x: Int, r: Int, offset: Int, arr: ArrayList[(Int, Int)]): Unit = {
-    println(arr)
-    println(offset)
 
-    if (x-r > 0 && x+r <= world.worldWidth-1) {
+    val low_within = x-r >= 0
+    val high_within = x+r <= world.worldWidth-1
+
+    if (low_within && high_within) {
       mergeAdd((offset + x-r, offset + x+r+1), arr)
-//      arr.add((offset + x-r, offset + x+r+1))
 
-    } else if (2 * r + 1 >= world.worldWidth) {
-      if (xWraps) {
-        mergeAdd((offset + 0, offset + world.worldWidth), arr)
-//        arr.add((offset + 0, offset + world.worldWidth))
-      } else {
-        mergeAdd((offset + (x - r).max(0), offset + (x + r + 1).min(world.worldWidth)), arr)
-//        arr.add((offset + (x - r).max(0), offset + (x + r + 1).min(world.worldWidth)))
-      }
+    } else if (!low_within && !high_within) {
+      mergeAdd((offset + 0, offset + world.worldWidth), arr)
 
-    } else if (x-r < 0) {
-      mergeAdd((offset + 0, x + r + 1), arr)
-//      arr.add((offset + 0, x + r + 1))
+    } else if (!low_within) {
+      mergeAdd((offset + 0, offset + x + r + 1), arr)
       if (xWraps) {
         mergeAdd((offset + world.worldWidth + x - r, offset + world.worldWidth), arr)
-//        arr.add((offset + world.worldWidth + x - r, offset + world.worldWidth))
       }
 
-    } else { // x + r >= world.worldWidth
+    } else { // !high_within
       if (xWraps) {
         mergeAdd((offset + 0, offset + x + r - world.worldWidth + 1), arr)
-//        arr.add((offset + 0, offset + x + r - world.worldWidth + 1))
       }
       mergeAdd((offset + x - r, offset + world.worldWidth), arr)
-//      arr.add((offset + x - r, offset + world.worldWidth))
     }
 
-    println(arr)
-    println()
   }
 
   def mergeAdd(value: (Int, Int), arr: ArrayList[(Int, Int)]): Unit = {
     val s = arr.size()
-    if (s == 0 || arr.get(s - 1)._2 != value._1) {
-      arr.add(value);
+    if (s == 0 || arr.get(s - 1)._2 < value._1) {
+      arr.add(value)
     } else {
       val last = arr.get(s - 1)
-      arr.set(s - 1, (last._1, value._2))
+      arr.set(s - 1, (last._1.min(value._1), value._2.max(last._2)))
     }
   }
 
   // 4 cases:
   // 1. x-r >=0 and x + r <= w-1
-  // 2. 2r + 1 >= w
+  // 2. x-r < 0 && x+r >= w
   // 3. x-r < 0
   // 4. x+r >= w
   
@@ -143,30 +129,27 @@ extends Neighbors {
     val ans: ArrayList[(Int, Int)] = new ArrayList()
     val a = Array((-1, -1), (-1, -1)) // at most 2 possible disjoint y ranges
 
-    if (y-r > 0 && y+r <= world.worldHeight-1) { // y range is completely within world
+    val low_within = y-r >= 0
+    val high_within = y+r <= world.worldHeight-1
+
+    if (low_within && high_within) { // y range is completely within world
       a(0) = (y-r, y+r+1)
 
-    } else if (2*r + 1 >= world.worldHeight) {
-      val b = ((y-r).max(0), (y+r+1).min(world.worldHeight))
-      if (yWraps) {
-        a(0) = (world.worldHeight+(y-r), world.worldHeight)
-        a(1) = b
-      } else {
-        a(0) = b
-      }
+    } else if (!low_within && !high_within) {
+      a(0) = (0, world.worldHeight)
 
-    } else if (y-r < 0) {
-      a(0) = (0,y+r+1)
-      if (yWraps) {
-        a(1) = (world.worldHeight + y - r, world.worldHeight)
-      }
-    } else {
-      val b = (y - r, world.worldHeight)
+    } else if (low_within) { // low_within && !high_within
+      val b = (y-r, world.worldHeight)
       if (yWraps) {
         a(0) = (0, y + r - world.worldHeight + 1)
         a(1) = b
       } else {
         a(0) = b
+      }
+    } else { // !low_within && high_within
+      a(0) = (0, y+r+1)
+      if (yWraps) {
+        a(1) = (world.worldHeight + y - r, world.worldHeight)
       }
     }
 
